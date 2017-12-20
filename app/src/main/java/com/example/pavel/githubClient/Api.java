165ede0ce23d0fabde4386082cdf9c2f6f4fb209
tempestme <paulsecondtwin@gmail.com>
2017-12-20 22:55:39 +0300

    app name fixed

--- app/src/main/java/com/example/pavel/githubClient/Api.java
+++ app/src/main/java/com/example/pavel/githubClient/Api.java
@@ -2,6 +2,8 @@ package com.example.pavel.githubClient;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 /**
@@ -11,6 +13,7 @@ import retrofit2.http.Path;
 public interface Api {
 
     String BASE_URL="https://api.github.com/users/";
+
 
     @GET("{user}")
     Call<User> GetUser(@Path("user") String user);
